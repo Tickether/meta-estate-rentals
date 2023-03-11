6962613304c2f145c71f14353146a6bfb2c0a854
Tickether <tickether@gmail.com>
2023-03-11 23:42:34 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { ConnectWalletProvider, getDefaultConnectors, ConnectButton, useConnectionModal } from '@shopify/connect-wallet';
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { sepolia, mainnet, polygon } from "wagmi/chains";
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+
 
 function App() {
+
+  const { chains, provider } = configureChains(
+    [sepolia, mainnet, polygon], 
+    [infuraProvider({ apiKey: "8231230ce0b44ec29c8682c1e47319f9" }), publicProvider()],
+
+  );
+
+  const {connectors} = getDefaultConnectors({chains})
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  });
+
+  const {openModal} = useConnectionModal()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WagmiConfig client={wagmiClient}>
+        <ConnectWalletProvider chains={chains}>
+          <div><ConnectButton/></div>
+          <div>
+            <button onClick={openModal}> connect </button>
+          </div>
+        </ConnectWalletProvider>
+      </WagmiConfig>
     </div>
   );
 }
